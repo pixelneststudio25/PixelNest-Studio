465973711287714edbcf80a5d0397843c07505c1
pixelneststudio25 <pixelneststudio25@gmail.com>
2025-09-09 00:07:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// Mobile Menu Toggle
-const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.querySelector(".nav-links");
-
-if (menuToggle) {
-  menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-  });
-}
 
 // Lightbox for Portfolio
 const galleryImages = document.querySelectorAll(".gallery img");
